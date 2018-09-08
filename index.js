68ea84d64af85383b6c12522f8b0c03ae2ac0162
Thomas Huang <thomaseh@umich.edu>
2018-09-08 15:14:15 -0400

    Added picture. Added publications. Changed text.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pub_down = false
 var exp_down = false
 var tae_down = false
 var proj_down = false
@@ -6,6 +7,7 @@ var misc_down = false
 function dispButton(button) {
     document.getElementById("res_exp").style.display = "none"
     document.getElementById("prof_exp").style.display = "none"
+    document.getElementById("pub").style.display = "none"
     document.getElementById("proj").style.display = "none"
     document.getElementById("teach").style.display = "none"
     document.getElementById("ed").style.display = "none"
@@ -13,6 +15,7 @@ function dispButton(button) {
     document.getElementById("read").style.display = "none"
     if (button === "exp") {
         exp_down = !exp_down
+        pub_down = false
         tae_down = false
         proj_down = false
         misc_down = false
@@ -25,9 +28,24 @@ function dispButton(button) {
             removeButton()
             document.getElementById("exp_but").classList.add("pressed")
         }
+    } else if (button == "pub") {
+        pub_down = !pub_down
+        exp_down = false
+        tae_down = false
+        proj_down = false
+        misc_down = false
+        if (!pub_down) {
+            showAll()
+            document.getElementById("pub_but").classList.remove("pressed")
+        } else {
+            document.getElementById("pub").style.display = "block"
+            removeButton()
+            document.getElementById("pub_but").classList.add("pressed")
+        }
     } else if (button === "tae") {
         tae_down = !tae_down
         exp_down = false
+        pub_down = false
         proj_down = false
         misc_down = false
         if (!tae_down) {
@@ -42,6 +60,7 @@ function dispButton(button) {
     } else if (button === "proj") {
         proj_down = !proj_down
         exp_down = false
+        pub_down = false
         tae_down = false
         misc_down = false
         if (!proj_down) {
@@ -55,6 +74,7 @@ function dispButton(button) {
     } else if (button === "misc") {
         misc_down = !misc_down
         exp_down = false
+        pub_down = false
         tae_down = false
         proj_down = false
         if (!misc_down) {
@@ -72,6 +92,7 @@ function dispButton(button) {
 function showAll() {
     document.getElementById("res_exp").style.display = "block"
     document.getElementById("prof_exp").style.display = "block"
+    document.getElementById("pub").style.display = "block"
     document.getElementById("proj").style.display = "block"
     document.getElementById("teach").style.display = "block"
     document.getElementById("ed").style.display = "block"
@@ -81,6 +102,7 @@ function showAll() {
 
 function removeButton() {
     document.getElementById("exp_but").classList.remove("pressed")
+    document.getElementById("pub_but").classList.remove("pressed")
     document.getElementById("tae_but").classList.remove("pressed")
     document.getElementById("proj_but").classList.remove("pressed")
     document.getElementById("misc_but").classList.remove("pressed")
